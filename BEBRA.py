d3bb91224abe2c0989dae279be2768f534dbd066
Illya-Viktorskij <viktorskij777@gmail.com>
2024-09-09 20:06:54 +0300

    Add files via upload

---
+++ BEBRA.py
@@ -0,0 +1,13 @@
+bebra ="Hello World"
+name ="Illya"              #створюєм дані
+surname ="Viktorskij"
+age =17
+
+print(bebra)
+print(name)
+print(surname) #Вивід в консоль
+print(age)
+
+print(type(name))
+print(type(surname))    #Визначеня типу
+print(type(age))
